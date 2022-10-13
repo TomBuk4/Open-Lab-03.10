34eb40c075728dfa1509b06ad64b20c0ba2ebdfc
TomBuk4 <104893605+TomBuk4@users.noreply.github.com>
2022-10-13 08:19:03 +0200

    Update Checker.cs

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._10
 {
@@ -6,7 +6,14 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            foreach (char c in str)
+            {
+                if (c == letter) 
+                a++;
+            }
+            
+            return a;
         }
     }
 }
